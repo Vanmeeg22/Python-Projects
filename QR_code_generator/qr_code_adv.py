7c275ccfb73f187e2dcc099238a4cda3f563d77f
Vanmeeganathan <vanmeegrkt@gmail.com>
2024-06-30 20:15:26 +0530

    9th commit

--- QR_code_generator/qr_code_adv.py
+++ QR_code_generator/qr_code_adv.py
@@ -2,10 +2,10 @@ import qrcode
 from PIL import Image
 import qrcode.constants
 
-qr = qrcode.QRCode(version = 1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 10, border = 4,)
+qr = qrcode.QRCode(version = 1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 10, border = 5,)
 
 msg = input("Enter your QR message here: ")
 qr.add_data(msg)
 qr.make(fit=True)
-img = qr.make_image(fill_color = "red", back_color = "black")
+img = qr.make_image(fill_color = "#00B7EB", back_color = "black")
 img.save("qr.png")
